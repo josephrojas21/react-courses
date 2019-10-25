2d650e8f130297470f6c3f2c6ffe8415bfbfb4a6
Joseph Rojas Montoya <joseph.rojas@se.com>
2019-10-25 16:54:48 -0500

    forms y events

--- envents-and-refs/src/App.js
+++ envents-and-refs/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super()
+    this.state = { muouex: 0, mouseY:0 }
+
+    //this.handleMouseMove = this.handleMouseMove.bind(this)
+  }
+
+  handleMouseMove = (e) =>{
+    const {clientY, clientX} = e;
+    this.setState({mouseX: clientX, mouseY: clientY})
+  }
+  
+  handleClick = (e) =>{
+    console.log(e);
+    alert('hh')
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <h4>Eventos</h4>
+        <button onClick={this.handleClick}>holaa</button>
+        <div onMouseMove={this.handleMouseMove} style={{border: '1px solid #000', marginTop: 10, padding: 10}}>
+          <p>{this.state.mouseX}, {this.state.mouseY}</p>
+        </div>
+      </div>
+    )
+  }
+
 }
 
 export default App;
